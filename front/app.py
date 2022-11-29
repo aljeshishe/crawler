f032d4041ee3890be47110ff184f3da03d5794aa
alexeygrachev <agrachev@cindicator.com>
2022-11-29 20:58:36 +0300

    ready for deploy

---
+++ front/app.py
@@ -0,0 +1,22 @@
+from dash import Dash, html
+from flask import Flask, jsonify, make_response
+
+
+app = Flask(__name__)
+
+
+
+@app.route("/hello")
+def hello():
+    return jsonify(message='Hello from path!')
+
+
+_app = Dash(
+    __name__,
+    server=app,
+)
+
+_app.layout = html.Div(html.H1("Hello Dash22!"))
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- worker/src/crawl_hh.py
+++ worker/src/crawl_hh.py
@@ -3,7 +3,6 @@ import logging
 import os
 import re
 from datetime import datetime
-
 import boto3
 import pandas as pd
 import requests
