3bfdc3fe3fa7b7ec2e028c07ae59f196403aeda9
alexeygrachev <agrachev@cindicator.com>
2022-12-04 22:44:51 +0300

    implemented front pdv

--- front/runner.py
+++ front/runner.py
@@ -1,23 +1,20 @@
-import base64
-from pathlib import Path
-
 import boto3
+import requests
 
 
-def invoke_function(function_name):
-    print(f"Executing function={function_name}")
-    r = client.invoke(FunctionName=function_name, LogType="Tail")
+def invoke_function(service_name):
+    client = boto3.client("apigatewayv2")
+    response = client.get_apis()
+    apis = response["Items"]
+    api = next(api for api in apis if api["Name"] == service_name)
+    url = api["ApiEndpoint"]
+    print(f"Calling {url}")
+    resp = requests.get(url)
+    resp.raise_for_status()
+    print(f"OK")
 
-    logs = base64.b64decode(r["LogResult"]).decode("utf-8").replace("\r", "\n")
-    print(f"Logs:\n{logs}")
-
-    payload = r["Payload"].read().decode("utf-8")
-    print(f"Payload:\n{payload}")
-    if r.get('FunctionError') == 'Unhandled':
-        print("Error!")
-        exit(-1)
 
 
 if __name__ == "__main__":
     client = boto3.client('lambda')
-    invoke_function("front-dev-api")
+    invoke_function("dev-front")
