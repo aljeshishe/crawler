fabf368deae47ca2e003a1b523ed342bdd996650
alexeygrachev <agrachev@cindicator.com>
2022-12-12 18:59:29 +0300

    renamed pdv file

---
+++ collector/pdv.py
@@ -0,0 +1,4 @@
+from common.utils import invoke_function
+
+if __name__ == "__main__":
+    invoke_function(function_name="collector-stage5-function")
---
+++ front/pdv.py
@@ -0,0 +1,4 @@
+from common.utils import invoke_api_endpoint
+
+if __name__ == "__main__":
+    invoke_api_endpoint("dev-front")
