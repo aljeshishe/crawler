8f7d52d8c1e31aa6dcb8b6b82853649e0266cc15
Your Name <you@example.com>
2023-03-01 20:39:47 +0200

    front: renamed front -> func

--- front/pdv.py
+++ front/pdv.py
@@ -1,4 +1,6 @@
-from common.utils import invoke_api_endpoint
+from common import utils
 
 if __name__ == "__main__":
-    invoke_api_endpoint("dev-front")
+    config = utils.dot_env_config()
+    stage = config["stage"]
+    utils.invoke_api_endpoint(f"{stage}-front")
